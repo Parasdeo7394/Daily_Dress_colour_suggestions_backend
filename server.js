21ef2c7d9cf8a50526cd1e9327debebdd74e1bdf
Parasdeo7394 <paras7394@gmail.com>
2023-09-14 14:19:17 +0530

    server file update

--- server.js
+++ server.js
@@ -1,3 +1,74 @@
+// import express from "express";
+// import colors from "colors";
+// import dotenv from "dotenv";
+// import morgan from "morgan";
+// import connectDB from "./config/db.js";
+// import authRoute from "./routes/authRoute.js";
+// import categoryRoutes from './routes/categoryRoutes.js';
+// import productRoutes from './routes/productRoutes.js';
+// import cors from 'cors';
+// import path from 'path';
+
+
+
+
+// // configure env
+// dotenv.config();
+
+// //databse config
+// connectDB();
+
+
+
+
+// // rest object
+// const app = express();
+
+
+// //middelwares
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan("dev"));
+// app.use(express.static(path.join(__dirname, './client/build')))
+
+
+
+
+
+// // routes
+// app.use("/api/v1/auth" , authRoute);
+// app.use("/api/v1/category",categoryRoutes);
+// app.use("/api/v1/product",productRoutes);
+
+
+
+
+
+
+// // rest api
+// // app.get('/', (req,res) =>{
+// //     res.send(
+// //         "<h1>Welcome to our full ecoomerce app</h1>"
+// //     )
+// // })
+
+// app.use('*',function(req,res){
+//    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+// });
+
+
+// // PORT
+// const PORT = process.env.PORT || 8080 ;
+
+// // run listen
+// app.listen(PORT, ()=>{
+//     console.log(`server is running on ${process.env.DEV_MODE} mode on port${PORT}`.bgCyan.white);
+// });
+
+
+////  "proxy": "http://localhost:8080",
+
+
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
@@ -8,62 +79,41 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 import path from 'path';
-
-
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 // configure env
 dotenv.config();
 
-//databse config
+// database config
 connectDB();
-
-
-
 
 // rest object
 const app = express();
 
-
-//middelwares
+// middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')))
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-
-
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // routes
-app.use("/api/v1/auth" , authRoute);
-app.use("/api/v1/category",categoryRoutes);
-app.use("/api/v1/product",productRoutes);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-
-
-
-
-
-// rest api
-// app.get('/', (req,res) =>{
-//     res.send(
-//         "<h1>Welcome to our full ecoomerce app</h1>"
-//     )
-// })
-
-app.use('*',function(req,res){
-   res.sendFile(path.join(__dirname,'./client/build/index.html'));
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-
 
 // PORT
-const PORT = process.env.PORT || 8080 ;
+const PORT = process.env.PORT || 8080;
 
 // run listen
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${process.env.DEV_MODE} mode on port${PORT}`.bgCyan.white);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
-
-
-////  "proxy": "http://localhost:8080",
